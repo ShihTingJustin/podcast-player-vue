1ca755981ab6060a46bcb25cd04df504595e3ccb
ShihTingJustin <justinhuang777@gmail.com>
2022-07-17 17:26:16 +0800

    feat: add rss data, add mode for condition rendering

--- src/components/HeaderItem.vue
+++ src/components/HeaderItem.vue
@@ -1,14 +1,19 @@
-<script setup lang="ts">
-// defineProps<{
-//   msg: string;
-// }>();
+<script lang="ts">
+export default {
+  name: "HeaderItem",
+  props: {
+    headerData: {},
+  },
+};
 </script>
 
 <template>
   <div class="wrapper">
     <div class="left">
-      <div>channel image</div>
-      <div>channel or episode name</div>
+      <div class="image">
+        <img :src="headerData.image" />
+      </div>
+      <div>{{ headerData.name }}</div>
     </div>
     <button>play button</button>
   </div>
@@ -24,28 +29,15 @@
     div:first-child {
       margin-right: 20px;
     }
-  }
-}
 
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
+    .image {
+      border: 2px solid rgb(200, 200, 200);
+      img {
+        max-width: 200px;
+        max-height: 200px;
+        object-fit: center;
+      }
+    }
   }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,64 @@
-<script setup lang="ts">
+<script lang="ts">
+import { onMounted, reactive } from "vue";
+import type { Podcast } from "@/interfaces/index";
+import { getRssData } from "@/service/index";
 import HeaderItem from "@/components/HeaderItem.vue";
+import ListItem from "@/components/ListItem.vue";
+import EpisodeItem from "@/components/EpisodeItem.vue";
+import PlayerItem from "@/components/PlayerItem.vue";
+
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    HeaderItem,
+    ListItem,
+    EpisodeItem,
+    PlayerItem,
+  },
+  data() {
+    return {
+      mode: "channel",
+      episodeDetail: {
+        name: "",
+        image: "",
+      },
+    };
+  },
+  setup() {
+    const data: { podcastData: Podcast } = reactive({
+      podcastData: {
+        channelName: "",
+        channelImage: "",
+        episode: [],
+        episodeMap: {},
+      },
+    });
+
+    onMounted(async () => {
+      const res = await getRssData();
+      if (res) data.podcastData = res;
+    });
+
+    return { data };
+  },
+});
 </script>
 
 <template>
   <main>
-    <HeaderItem />
-    <div>home page</div>
+    <HeaderItem
+      :headerData="{
+        name: data.podcastData.channelName,
+        image: data.podcastData.channelImage,
+      }"
+    />
+    <div v-if="mode === 'channel'">
+      <ListItem />
+    </div>
+    <div v-if="mode === 'episode'">
+      <EpisodeItem />
+      <PlayerItem />
+    </div>
   </main>
 </template>
