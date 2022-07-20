cc2c4f7f76f360033841707a7e6d425b5b7b43ad
ShihTingJustin <justinhuang777@gmail.com>
2022-07-20 22:10:34 +0800

    chore: add primevue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/bootstrap4-light-blue/theme.css";
 
 import App from "./App.vue";
 import router from "./router";
@@ -10,5 +12,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(PrimeVue);
 
 app.mount("#app");
